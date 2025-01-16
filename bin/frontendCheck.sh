61f7735437d783d6ee0ce4ec6fefa51b08724d04
Ondřej Kühnel <ondrej@3brs.com>
2025-01-16 14:56:53 +0100

    fixup! fixup! fixup! js lint

---
+++ bin/frontendCheck.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# project root
+cd "$(dirname "$DIR")"
+
+cd svelteUi
+yarn install
+yarn run lint
+yarn run check
